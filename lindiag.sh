b66cda6e242df69e1fc9cc5ba9e2f9709798bdb8
MorpheusArch <admin@morpheusarch.co.uk>
2018-05-24 16:27:52 +0100

    Update lindiag.sh

--- lindiag.sh
+++ lindiag.sh
@@ -13,6 +13,10 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#Author
+#Mark Chisholm
+#http://morpheusarch.co.uk 
+
 ########################################################################
 #-- authCheck asks for adminstrator aka root priveleges
 
